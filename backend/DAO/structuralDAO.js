f3e00098d2a10e59480fe39e7ca68334d2d56261
Alice <alicecasali96@gmail.com>
2021-02-24 12:51:31 +0100

    [Backend] - Created structuralDAO with newStructural and getStructural

---
+++ backend/DAO/structuralDAO.js
@@ -0,0 +1,43 @@
+const db = require("../utils/db");
+const T = require("../utils/tablesDefinition");
+const E = require("../utils/customError");
+const U = require("../utils/utils");
+
+module.exports = {
+    insertStructural: async (i) => {
+        let res = await db(T.structuralTable)
+        .returning()
+        .insert({
+            userId: i.userId,
+            component: i.component,
+            pathology: i.pathology,
+            typology: i.typology,
+            notes: i.notes,
+            images: i.images,
+            latitude: i.latitude,
+            longitude: i.longitude,
+            place_tag: i.place_tag
+        }, ["id", "userId", "component", "pathology", "typology", "notes", "images", "latitude", "longitude", "created", "place_tag"])
+        .catch(e => {
+            if (e) {
+                console.error(`Message: ${e.message}`);
+                console.error(`Stack: ${e.stack}`);
+                throw new E.CustomError(E.InternalServerError, "Something went wrong while trying to save the impression")
+            }
+        });
+        return res[0];
+    },
+    getStructuralsByUserId: async (userId) => {
+        let res = await db(T.structuralTable)
+        .select()
+        .where("userId", userId)
+        .catch(e => {
+            if(e) {
+                console.error(`Message: ${e.message}`);
+                console.error(`Stack: ${e.stack}`);
+                throw new E.CustomError(E.InternalServerError, "Something went wrong while trying to retrieve impressions by userId");
+            }
+        });
+        return res;
+    },
+}
