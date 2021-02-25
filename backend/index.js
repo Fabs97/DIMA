f9488531d0dfe97d417ac674f33734b6c7733f8d
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2021-02-25 14:24:51 +0100

    [Backend] - Logger layer

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ const swaggerUI = require("swagger-ui-express");
 const APIDocumentation = require("./docs/openapi");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const L = require("./utils/logger");
 
-app.use(cors());
+app.use(cors());1
 
 app.use("/apis", swaggerUI.serve, swaggerUI.setup(APIDocumentation));
 app.use(express.json());
+app.use(L);
 
 const userRoute = require("./routes/userRoute");
 const impressionRoute = require("./routes/impressionRoute");
