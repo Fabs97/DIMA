26426ed08f34d486480018a7fd10299b9cee6344
Alice <alicecasali96@gmail.com>
2021-03-04 20:45:17 +0100

    [Frontend] - First phase structural form

---
+++ citylife/lib/screens/impressions/structural/local_widget/structuralForm.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class StructuralForm extends StatefulWidget {
+  @override
+  _StructuralFormState createState() => _StructuralFormState();
+}
+
+class _StructuralFormState extends State<StructuralForm> {
+  @override
+  Widget build(BuildContext context) {}
+}
