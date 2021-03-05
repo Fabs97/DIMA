dc9b91a0402fde6c8d8054a60f1c581c2b48b97e
Alice <alicecasali96@gmail.com>
2021-03-05 21:33:42 +0100

    [Frontend] - Updated structural impression steps

--- citylife/lib/screens/impressions/structural/structuralImpression.dart
+++ citylife/lib/screens/impressions/structural/structuralImpression.dart
@@ -1,8 +1,10 @@
 import 'package:citylife/models/cl_impression.dart';
 import 'package:citylife/models/cl_structural.dart';
 import 'package:citylife/screens/homepage/homepage.dart';
+import 'package:citylife/screens/impressions/structural/local_widget/structuralForm.dart';
 import 'package:citylife/utils/theme.dart';
 import 'package:citylife/widgets/littleMap.dart';
+import 'package:citylife/widgets/sharedForm.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,13 +17,12 @@ class StructuralImpression extends StatefulWidget {
 }
 
 class _StructuralImpressionState extends State<StructuralImpression> {
-  final List<Widget> steps = [Text("1"), Text("2"), Text("3")];
+  final List<Widget> steps = [StructuralForm(), SharedForm()];
   int selectedStep = 0;
   int nbSteps = 4;
 
   @override
   Widget build(BuildContext context) {
-    //final structuralImpression = context.watch<Structural>();
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: CLStructural()),
@@ -41,59 +42,71 @@ class _StructuralImpressionState extends State<StructuralImpression> {
             ],
           ),
           body: LayoutBuilder(
-            builder: (context, constraints) => Container(
-              width: constraints.maxWidth,
-              height: constraints.maxHeight,
-              child: Column(
-                children: [
-                  Container(
-                      height: constraints.maxHeight * 0.4, child: LittleMap()),
-                  Divider(),
-                  steps[selectedStep],
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      StepsIndicator(
-                        selectedStep: selectedStep,
-                        nbSteps: nbSteps,
-                        unselectedStepColorIn: T.emotionalColor,
-                        unselectedStepColorOut: T.emotionalColor,
-                        selectedStepColorIn: T.structuralColor,
-                        selectedStepColorOut: T.structuralColor,
-                        doneLineColor: T.emotionalColor,
-                        doneStepColor: T.emotionalColor,
-                        undoneLineColor: T.emotionalColor,
-                        doneStepSize: 13,
-                        unselectedStepSize: 13,
-                        selectedStepSize: 13,
-                        lineLength: 40,
-                        enableLineAnimation: true,
-                        enableStepAnimation: true,
+            builder: (context, constraints) => SingleChildScrollView(
+              child: Container(
+                width: constraints.maxWidth,
+                height: constraints.maxHeight,
+                child: Column(
+                  children: [
+                    Container(
+                        height: constraints.maxHeight * 0.4,
+                        child: LittleMap()),
+                    Container(
+                      width: constraints.maxWidth * 0.7,
+                      child: Divider(
+                        height: 50,
+                        thickness: 3,
+                        color: T.textDarkColor,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 45),
-                        child: MaterialButton(
-                          color: T.primaryColor,
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(20.0),
-                          ),
-                          onPressed: () {
-                            if (selectedStep < nbSteps) {
-                              setState(() {
-                                selectedStep++;
-                              });
-                            }
-                          },
-                          child: Text(
-                            'Next',
-                            style: TextStyle(color: T.textLightColor),
+                    ),
+                    Container(
+                        height: constraints.maxHeight * 0.4,
+                        child: steps[selectedStep]),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        StepsIndicator(
+                          selectedStep: selectedStep,
+                          nbSteps: nbSteps,
+                          unselectedStepColorIn: T.emotionalColor,
+                          unselectedStepColorOut: T.emotionalColor,
+                          selectedStepColorIn: T.structuralColor,
+                          selectedStepColorOut: T.structuralColor,
+                          doneLineColor: T.emotionalColor,
+                          doneStepColor: T.emotionalColor,
+                          undoneLineColor: T.emotionalColor,
+                          doneStepSize: 13,
+                          unselectedStepSize: 13,
+                          selectedStepSize: 13,
+                          lineLength: 40,
+                          enableLineAnimation: true,
+                          enableStepAnimation: true,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 45),
+                          child: MaterialButton(
+                            color: T.primaryColor,
+                            shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(20.0),
+                            ),
+                            onPressed: () {
+                              if (selectedStep < nbSteps) {
+                                setState(() {
+                                  selectedStep++;
+                                });
+                              }
+                            },
+                            child: Text(
+                              'Next',
+                              style: TextStyle(color: T.textLightColor),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
