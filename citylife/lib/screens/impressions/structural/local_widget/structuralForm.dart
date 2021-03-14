cc420bec73b8b1c46351c193c2e22e5f2c494f52
Alice <alicecasali96@gmail.com>
2021-03-14 11:58:56 +0100

    [Frontend] - Fixed UI structural form

--- citylife/lib/screens/impressions/structural/local_widget/structuralForm.dart
+++ citylife/lib/screens/impressions/structural/local_widget/structuralForm.dart
@@ -1,4 +1,5 @@
 import 'package:citylife/models/cl_structural.dart';
+import 'package:citylife/utils/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -20,63 +21,78 @@ class _StructuralFormState extends State<StructuralForm> {
         child: SingleChildScrollView(
           child: Consumer<CLStructural>(
             builder: (context, structuralImpression, _) => Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.domain),
-                    hintText: "Component",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.domain,
+                        color: T.primaryColor,
+                      ),
+                      hintText: "Component",
+                    ),
+                    initialValue: structuralImpression.component ?? "",
+                    onChanged: (value) {
+                      setState(() {
+                        structuralImpression.component = value;
+                      });
+                    },
                   ),
-                  initialValue: structuralImpression.component ?? "",
-                  onChanged: (value) {
-                    setState(() {
-                      structuralImpression.component = value;
-                    });
-                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.help_outline),
-                    hintText: "Pathology",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.help_outline,
+                        color: T.primaryColor,
+                      ),
+                      hintText: "Pathology",
+                    ),
+                    initialValue: structuralImpression.pathology ?? "",
+                    onChanged: (value) {
+                      setState(() {
+                        structuralImpression.pathology = value;
+                      });
+                    },
                   ),
-                  initialValue: structuralImpression.pathology ?? "",
-                  onChanged: (value) {
-                    setState(() {
-                      structuralImpression.pathology = value;
-                    });
-                  },
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    border: Border.all(color: Colors.grey[300], width: 1.0),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButton<String>(
-                      isExpanded: true,
-                      items: [
-                        DropdownMenuItem<String>(
-                          child: Text('Fixing'),
-                          value: 'Fixing',
-                        ),
-                        DropdownMenuItem<String>(
-                          child: Text('Add new'),
-                          value: 'Add new',
-                        ),
-                        DropdownMenuItem<String>(
-                          child: Text('Replace'),
-                          value: 'Replace',
-                        ),
-                      ],
-                      onChanged: (value) {
-                        setState(() {
-                          structuralImpression.typology = value;
-                          _value = value;
-                        });
-                      },
-                      hint: Text('Type of Intervention'),
-                      value: _value,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      border: Border.all(color: Colors.grey[300], width: 1.0),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        items: [
+                          DropdownMenuItem<String>(
+                            child: Text('Fixing'),
+                            value: 'Fixing',
+                          ),
+                          DropdownMenuItem<String>(
+                            child: Text('Add new'),
+                            value: 'Add new',
+                          ),
+                          DropdownMenuItem<String>(
+                            child: Text('Replace'),
+                            value: 'Replace',
+                          ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            structuralImpression.typology = value;
+                            _value = value;
+                          });
+                        },
+                        hint: Text('Type of Intervention'),
+                        value: _value,
+                      ),
                     ),
                   ),
                 ),
