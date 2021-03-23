b1b614c8eaeb86c9138d37d7536b1bafe4efd74e
Alice <alicecasali96@gmail.com>
2021-03-23 10:24:01 +0100

    [Frontend] - Added provider to home

--- citylife/lib/screens/home/home.dart
+++ citylife/lib/screens/home/home.dart
@@ -1,7 +1,20 @@
+import 'package:citylife/models/cl_impression.dart';
+import 'package:citylife/screens/home/local_widgets/my_markers_state.dart';
+import 'package:citylife/services/api_services/impressions_api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:latlong/latlong.dart' as ll;
 import 'package:location/location.dart';
+import 'package:provider/provider.dart';
+
+class HomeArguments {
+  final double latMin;
+  final double latMax;
+  final double longMin;
+  final double longMax;
+
+  HomeArguments(this.latMin, this.latMax, this.longMin, this.longMax);
+}
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -11,6 +24,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  HomeArguments args;
   LatLng _center = LatLng(20.5937, 78.9629);
   GoogleMapController _controller;
   Location _location = new Location();
@@ -72,51 +86,58 @@ class _HomeState extends State<Home> {
       width: MediaQuery.of(context).size.width,
       child: Stack(
         children: [
-          GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: _zoom,
+          Consumer<MyMarkersState>(
+            builder: (_, state, __) => GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: _zoom,
+              ),
+              mapType: MapType.normal,
+              myLocationEnabled: true,
+              tiltGesturesEnabled: false,
+              myLocationButtonEnabled: true,
+              onMapCreated: _onMapCreated,
+              markers: state.markers,
+              circles: _circles,
+              onCameraIdle: () {
+                if (_controller != null) {
+                  _controller.getVisibleRegion().then((bounds) async {
+                    LatLng northEast = bounds.northeast;
+                    LatLng southWest = bounds.southwest;
+
+                    final distance = ll.DistanceHaversine().as(
+                          ll.LengthUnit.Meter,
+                          ll.LatLng(northEast.latitude, northEast.longitude),
+                          new ll.LatLng(
+                              northEast.latitude, southWest.longitude),
+                        ) /
+                        2;
+
+                    args = HomeArguments(southWest.latitude, northEast.latitude,
+                        southWest.longitude, northEast.longitude);
+
+                    state.impressions = await ImpressionsAPIService.route(
+                        "/byLatLong",
+                        urlArgs: args);
+
+                    // _circles = Set.from([
+                    //   Circle(
+                    //       circleId: CircleId("myCircle"),
+                    //       radius: distance * .95,
+                    //       center: _center,
+                    //       fillColor: Colors.amber,
+                    //       strokeColor: Colors.amber,
+                    //       onTap: () {
+                    //         print('circle pressed');
+                    //       })
+                    // ]);
+                  });
+                }
+              },
             ),
-            mapType: MapType.normal,
-            myLocationEnabled: true,
-            tiltGesturesEnabled: false,
-            myLocationButtonEnabled: true,
-            onMapCreated: _onMapCreated,
-            onCameraIdle: _onCameraIde,
-            circles: _circles,
           ),
         ],
       ),
     );
-  }
-
-  void _onCameraIde() {
-    if (_controller != null) {
-      _controller.getVisibleRegion().then((bounds) {
-        LatLng northEast = bounds.northeast;
-        LatLng southWest = bounds.southwest;
-
-        final distance = ll.DistanceHaversine().as(
-              ll.LengthUnit.Meter,
-              ll.LatLng(northEast.latitude, northEast.longitude),
-              new ll.LatLng(northEast.latitude, southWest.longitude),
-            ) /
-            2;
-
-        setState(() {
-          _circles = Set.from([
-            Circle(
-                circleId: CircleId("myCircle"),
-                radius: distance * .95,
-                center: _center,
-                fillColor: Colors.amber,
-                strokeColor: Colors.amber,
-                onTap: () {
-                  print('circle pressed');
-                })
-          ]);
-        });
-      });
-    }
   }
 }
