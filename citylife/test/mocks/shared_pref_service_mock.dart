315e009d639c5574968b38dc7f936d8c2c62c89c
Fabrizio Siciliano <fabrizio.siciliano@mail.polimi.it>
2021-04-14 10:38:22 +0200

    [Testing] - SharedPrefService
    100% coverage

--- citylife/test/mocks/shared_pref_service_mock.dart
+++ citylife/test/mocks/shared_pref_service_mock.dart
@@ -1,4 +1,6 @@
 import 'package:citylife/services/shared_pref_service.dart';
 import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MockSharedPrefService extends Mock implements SharedPrefService {}
+class MockSharedPreferences extends Mock implements SharedPreferences {}
